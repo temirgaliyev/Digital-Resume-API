5c1c3ee77dff3109cfff6d0bff7c8e3e918fcbea
Temirgaliyev Yelmurat <temirgalievelmurat@gmail.com>
2020-04-29 00:52:15 +0600

    Add resume api

--- api/utils.py
+++ api/utils.py
@@ -1,7 +1,9 @@
 from hashlib import md5, sha256
 from flask import abort
+
 from mongoengine import Document 
 from mongoengine.queryset import QuerySet
+from bson.objectid import ObjectId
 
 
 def custom_hash(string):
@@ -13,7 +15,7 @@ def custom_hash(string):
 def abort_if_invalid_request_params(_dict, attrs):
 	for attr in attrs:
 		if attr not in _dict:
-			abort(400, message=f"Request must contain '{attr}'")
+			abort(400, f"Request must contain '{attr}'")
 
 
 def me_obj_to_serializable(obj, need_id=True):
@@ -23,8 +25,12 @@ def me_obj_to_serializable(obj, need_id=True):
 	elif isinstance(obj, QuerySet):
 		return [me_document_to_dict(val, need_id) for val in obj]
 
+	elif isinstance(obj, list):
+		return [me_document_to_dict(_id, need_id) for _id in obj]
+
 	else:
-		abort(400, message=f"'obj' type '{type(obj)}' is incorrect")
+		abort(400, f"'obj' type '{type(obj)}' is incorrect")
+
 
 def me_document_to_dict(obj, need_id=True):
 	if isinstance(obj, Document):
@@ -33,6 +39,12 @@ def me_document_to_dict(obj, need_id=True):
 			obj_dict['id'] = str(obj_dict.pop('_id'))
 		else:
 			del obj_dict['_id']
+
+		for key, value in obj_dict.items():
+			if isinstance(value, ObjectId):
+				obj_dict[key] = str(value)
+			if isinstance(value, list):
+				obj_dict[key] = [str(_id) for _id in value]
 
 		return obj_dict
 	else:
@@ -45,7 +57,7 @@ def exception_decorator(errors=(Exception, )):
 
         def new_func(*args, **kwargs):
             try:
-                return func(*args, **kwargs), 200
+                return func(*args, **kwargs)
             except errors as e:
                 return {"err": str(e)}, 400
 
--- api/v1/controller.py
+++ api/v1/controller.py
@@ -7,3 +7,22 @@ controller = Blueprint('v1', __name__)
 api = Api(controller)
 
 api.add_resource(UserApi, '/user', '/user/')
+api.add_resource(ExperienceApi, '/experience', '/experience/')
+api.add_resource(EducationApi, '/education', '/education/')
+api.add_resource(CertificateApi, '/certificate', '/certificate/')
+api.add_resource(MedcardApi, '/medcard', '/medcard/')
+
+api.add_resource(ResumeApi, '/resume', '/resume/')
+api.add_resource(ResumeCertificatesApi, '/resume/certificates', '/resume/certificates/')
+
+# '/resume', '/resume/'
+		# /create (standart)
+		# /delete (many by user, one by id)
+		# /get (many by user, one by id) 
+		# /update (summary)
+
+
+# /resume/experiences?id # append/delete
+# /resume/{id}/educations # append/delete
+# /resume/{id}/medcards # append/delete
+# /resume/{id}/certificates # append/delete
